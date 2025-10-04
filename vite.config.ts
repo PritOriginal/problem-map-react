e3771d13d05627f4954660a91b912a4fdc40fb5d
Махнатеев Степан <stepan01062004@mail.ru.>
2025-10-04 17:51:11 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
-export default defineConfig({
-  server: {
-    host: 'problem-map.pritoriginal.ru'
-  },
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    server: {
+      host: env.APP_HOST,
+      port: Number(env.APP_PORT),
+      proxy: {
+        '/api': {
+          target: `http://${env.API_HOST}:${env.API_PORT}`,
+          rewrite: (path) => path.replace(/^\/api/, '/'),
+        }
+      }
+    },
+    plugins: [react()],
+  }
 })

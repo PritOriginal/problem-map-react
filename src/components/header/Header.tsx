6c90adc80f298e67862eb35e86cbff4cba697e0b
Махнатеев Степан <stepan01062004@mail.ru.>
2026-02-02 01:27:22 +0300

    Add auth methods in header for users

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,18 +1,36 @@
+import { Link } from "react-router-dom";
 import "./Header.scss"
+import user from "../../store/user";
+import { observer } from "mobx-react-lite";
 
-export default function Header() {
+const Header = observer(function Header() {
     return (
         <header>
             <div className="header-container">
-                <div style={{display: "flex", gap: "32px"}}>
+                <div style={{ display: "flex", gap: "32px" }}>
                     <p>Карта проблем</p>
-                    <p style={{color: "#bdbdbdff"}}>Задания</p>
-                    <p style={{color: "#bdbdbdff"}}>Рейтинг</p>
+                    <p style={{ color: "#bdbdbdff" }}>Задания</p>
+                    <p style={{ color: "#bdbdbdff" }}>Рейтинг</p>
                 </div>
-                <div>
-                    <p style={{color: "#bdbdbdff"}}>Логин</p>
+                <div className="header-container__user-info">
+                    {user.id !== 0 ?
+                        <Link to={"/signout"}>
+                            <p>{user.username}</p>
+                        </Link>
+                        :
+                        <>
+                            <Link to={"/signin"}>
+                                <p>Войти</p>
+                            </Link>
+                            <Link to={"/signup"}>
+                                <p>Зарегистрироваться</p>
+                            </Link>
+                        </>
+                    }
                 </div>
             </div>
         </header>
     );
-}
+});
+
+export default Header;

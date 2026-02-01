4521823da7a89fdc968b32aca97d2a3d5cf471d1
Махнатеев Степан <stepan01062004@mail.ru.>
2026-02-02 01:16:59 +0300

    Update add problem panel - Add call to the service method to create a new mark - Add SelectFiles component

--- src/components/panel/panels/add-problem.tsx
+++ src/components/panel/panels/add-problem.tsx
@@ -1,26 +1,33 @@
-import { useEffect, useState } from "react";
-import MarksService, { MarkType } from "../../../services/MarksService";
+import { ChangeEvent, useEffect, useId, useState } from "react";
+import MarksService, { AddMarkRequest, MarkType } from "../../../services/MarksService";
 import Select from 'react-select';
 import { Button } from "../../button/button";
+import { observer } from "mobx-react-lite";
+import selectedPoint from "../../../store/selected_point";
+import { useNavigate } from "react-router-dom";
+import SelectFiles from "../../SelectFiles";
 
+const AddProblem = observer(function AddProblem() {
+    const navigate = useNavigate();
 
-export default function AddProblem() {
     const [markTypes, setMarkTypes] = useState<MarkType[]>([])
 
     const markTypesOptions = markTypes.map((type) => ({
-        value: type.type_mark_id,
+        value: type.mark_type_id,
         label: type.name,
     }))
 
     const [selectedMarkType, setSelectedMarkType] = useState<undefined | MarkType>(undefined)
-    const selectedMarkTypeOption = markTypesOptions.find(v => v.value === selectedMarkType?.type_mark_id);
+    const selectedMarkTypeOption = markTypesOptions.find(v => v.value === selectedMarkType?.mark_type_id);
 
     const [description, setDescription] = useState("")
+    const [photos, setPhotos] = useState<File[]>([]);
+
 
     useEffect(() => {
         MarksService.getMarkTypes()
             .then((data) => {
-                console.log(data.payload.mark);
+                console.log(data.payload.mark_types);
                 setMarkTypes(data.payload.mark_types)
             })
             .catch((error) => {
@@ -28,13 +35,42 @@ export default function AddProblem() {
             })
     }, [])
 
+    const onSelectedFile = (files: File[]) => {
+        setPhotos(files);
+    }
+
+    const addMark = () => {
+        if (selectedMarkType && photos.length > 0) {
+            var req: AddMarkRequest = {
+                point: {
+                    longitude: selectedPoint.coords[1],
+                    latitude: selectedPoint.coords[0]
+                },
+                mark_type_id: selectedMarkType.mark_type_id,
+                description: description
+            }
+
+            MarksService.addMark(req, photos)
+                .then((data) => {
+                    console.log(data.payload);
+                    navigate(`/`) // TODO: redirect to the mark page
+                })
+                .catch((error) => {
+                    console.log(error);
+                }) 
+        }
+    }
+
     return (
         <>
             <div className="panel__header">
                 <p style={{ fontSize: 18 }}>Отметить проблему</p>
             </div>
             <div className="panel__content">
-                <p style={{ fontSize: 12 }}>Координаты: 40.96110892973163, 52.54600597551669</p>
+                {/* <p>Вы не авторизаваны</p>
+                <p>Для того чтобы отметить проблему авторизуйтесь или создайте новый аккаунт</p> */}
+
+                <p style={{ fontSize: 12 }}>Координаты: {selectedPoint.coords[1]}, {selectedPoint.coords[0]}</p>
 
                 <p><b>Категория</b></p>
                 <Select
@@ -53,15 +89,16 @@ export default function AddProblem() {
                 ></textarea>
                 <p><b>Фотографии</b></p>
                 <div style={{ display: "flex", gap: "8px" }}>
-                    <div className="photo add">+</div>
-                    <div className="photo"></div>
+                    <SelectFiles onSelectedFiles={onSelectedFile} />
                 </div>
                 <div>
-                    <Button style="white-2-black">
+                    <Button style="white-2-black" onClick={addMark}>
                         <p>Отметить</p>
                     </Button>
                 </div>
             </div>
         </>
     )
-}
+});
+
+export default AddProblem;

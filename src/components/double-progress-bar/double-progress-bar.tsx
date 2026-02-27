17412a3f80426ffb6df9d202c49cf13d9f04cec9
Махнатеев Степан <stepan01062004@mail.ru.>
2026-02-27 11:22:00 +0300

    Add signatures in double progress bar

--- src/components/double-progress-bar/double-progress-bar.tsx
+++ src/components/double-progress-bar/double-progress-bar.tsx
@@ -3,19 +3,25 @@ import "./double-progress-bar.scss"
 
 const DoubleProgressBar = ({ negative, positive }: { negative: number, positive: number }) => {
     let sum = negative + positive;
-    if (sum < 5) {
-        sum = 5
+    if (sum < 3) {
+        sum = 3
     }
 
     return (
         <div className="double-progress-bar">
-            <div className="double-progress-bar__left__zone"></div>
-            <div className="double-progress-bar__left" style={{ width: `${Math.min(1, negative / sum / 0.7) * 50}%` }}>
-                <span className="double-progress-bar__left__text">{negative}</span>
+            <div className="double-progress-bar__scale">
+                <div className="double-progress-bar__scale__left__zone"></div>
+                <div className="double-progress-bar__scale__left" style={{ width: `${Math.min(1, negative / sum) * 50}%` }}>
+                    <span className="double-progress-bar__scale__left__text">{negative}</span>
+                </div>
+                <div className="double-progress-bar__scale__right__zone"></div>
+                <div className="double-progress-bar__scale__right" style={{ width: `${Math.min(1, positive / sum) * 50}%` }}>
+                    <span className="double-progress-bar__scale__right__text">{positive}</span>
+                </div>
             </div>
-            <div className="double-progress-bar__right__zone"></div>
-            <div className="double-progress-bar__right" style={{ width: `${Math.min(1, positive / sum / 0.7) * 50}%` }}>
-                <span className="double-progress-bar__right__text">{positive}</span>
+            <div className="double-progress-bar__signatures">
+                <p>Опровергли</p>
+                <p>Подтвердили</p>
             </div>
         </div>
     );

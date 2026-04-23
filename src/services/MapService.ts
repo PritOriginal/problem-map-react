1b415e2720bfe914de7f9d85b1ba48efbf48cba5
Махнатеев Степан <stepan01062004@mail.ru>
2026-04-23 22:50:12 +0300

    feat: add admin boundaries methods to MapService

--- src/services/MapService.ts
+++ src/services/MapService.ts
@@ -1,6 +1,67 @@
-import BaseService from "./BaseService"
+import BaseService, { IResponse } from "./BaseService"
+import { MultiPolygonGeometry } from "@yandex/ymaps3-types";
+
+export interface AdminBoundary {
+    id: number;
+    name: string;
+    admin_level: number;
+    geom: MultiPolygonGeometry;    
+}
+
+export interface GetAdminBoundariesRequest {
+    admin_levels: number[];    
+}
+
+export interface GetAdminBoundariesResponse extends IResponse {
+    payload: GetAdminBoundariesResponsePayload;
+}
+
+export interface GetAdminBoundariesResponsePayload {
+    admin_boundaries: AdminBoundary[]
+}
+
+export interface AdminBoundaryMarksCount {
+    id: number;
+    name: string;
+    total_count: number;
+    unconfirmed_count: number;
+    confirmed_count: number;
+    under_review_count: number;
+    closed_count: number;
+}
+
+export interface GetAdminBoundariesMarksCountRequest {
+    admin_levels: number[];    
+}
+
+export interface GetAdminBoundariesMarksCountResponse extends IResponse {
+    payload: GetAdminBoundariesMarksCountResponsePayload;
+}
+
+export interface GetAdminBoundariesMarksCountResponsePayload {
+    admin_boundaries: AdminBoundaryMarksCount[]
+}
+
 
 class MapService extends BaseService {
+    public getAdminBoundaries(req: GetAdminBoundariesRequest): Promise<GetAdminBoundariesResponse> {
+        const params = new URLSearchParams();
+        req.admin_levels.forEach(level => {
+            params.append("admin_levels", level.toString())
+        });
+
+        return fetch(`/api/map/admin-boundaries?${params}`).then(this.getResponse);
+    }
+
+     public getAdminBoundariesMarksCount(req: GetAdminBoundariesMarksCountRequest): Promise<GetAdminBoundariesMarksCountResponse> {
+        const params = new URLSearchParams();
+        req.admin_levels.forEach(level => {
+            params.append("admin_levels", level.toString())
+        });
+
+        return fetch(`/api/map/admin-boundaries/marks/count?${params}`).then(this.getResponse);
+    }
+
     public getDistricts() {
         return fetch("/api/map/districts").then(this.getResponse)
     }

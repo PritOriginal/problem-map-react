624df9d2bad5f894dd42dfa32f72488c129cfb01
Махнатеев Степан <stepan01062004@mail.ru>
2026-04-29 22:22:11 +0300

    feat: add get marks filters to marks store

--- src/store/marks.ts
+++ src/store/marks.ts
@@ -1,8 +1,19 @@
 import { makeAutoObservable, runInAction } from 'mobx';
-import MarksService, { Mark } from '../services/MarksService';
+import MarksService, { GetMarksRequest, Mark, MarkStatusType } from '../services/MarksService';
 
 class MarksStore {
     marks: Mark[] = [];
+    filters: GetMarksRequest = {
+        mark_type_ids: [],
+        mark_status_ids: [
+            MarkStatusType.UnconfirmedStatus,
+            MarkStatusType.ConfirmedStatus,
+            MarkStatusType.UnderReviewStatus,
+            MarkStatusType.RediscoveredStatus,
+            MarkStatusType.ClosedStatus,
+        ]
+    };
+
     isLoading: boolean = false;
     error: string | null = null;
 
@@ -14,7 +25,7 @@ class MarksStore {
         this.isLoading = true;
         this.error = null;
         try {
-            const response = await MarksService.getMarks();
+            const response = await MarksService.getMarks(this.filters);
             runInAction(() => {
                 this.marks = response.payload.marks;
                 this.isLoading = false;
@@ -24,6 +35,26 @@ class MarksStore {
             this.error = 'Ошибка загрузки меток';
             this.isLoading = false;
         }
+    }
+
+    updateMarkType = (markTypeId: number) => {
+        const index = this.filters.mark_type_ids.indexOf(markTypeId);
+        if (index !== -1) {
+            this.filters.mark_type_ids.splice(index, 1);
+        } else {
+            this.filters.mark_type_ids.push(markTypeId);
+        }
+        this.fetch();
+    }
+
+    updateMarkStatus = (markStatusId: number) => {
+        const index = this.filters.mark_status_ids.indexOf(markStatusId);
+        if (index !== -1) {
+            this.filters.mark_status_ids.splice(index, 1);
+        } else {
+            this.filters.mark_status_ids.push(markStatusId);
+        }
+        this.fetch();
     }
 }
 

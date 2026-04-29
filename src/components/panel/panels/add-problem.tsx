dd47a13baf6bf26b544a39e308e487c37f4cf134
Махнатеев Степан <stepan01062004@mail.ru>
2026-04-29 23:28:52 +0300

    refactor: remove unnecessary dependency

--- src/components/panel/panels/add-problem.tsx
+++ src/components/panel/panels/add-problem.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import MarksService, { AddMarkRequest, MarkType } from "../../../services/MarksService";
 import Select from 'react-select';
 import { Button } from "../../button/button";

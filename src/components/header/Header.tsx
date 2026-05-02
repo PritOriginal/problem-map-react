eb323942a45b96bf884b982c684070601beb3b84
Махнатеев Степан <stepan01062004@mail.ru>
2026-05-03 00:29:34 +0300

    feat: add responsive design for header - Add map icon - Add profile icon

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,28 +3,50 @@ import "./Header.scss"
 import user from "../../store/user";
 import { observer } from "mobx-react-lite";
 
+import ProfileIcon from "../../assets/profile.svg?react"
+import MapIcon from "../../assets/map.svg?react"
+import { useDeviceDetect } from "../../utils/hooks";
+
 const Header = observer(function Header() {
+    const { isMobile } = useDeviceDetect();
+
     return (
         <header>
             <div className="header-container">
-                <div style={{ display: "flex", gap: "32px" }}>
-                    <p>Карта проблем</p>
+                <div className="header-container__menu">
+                    <MapIcon style={{ fill: "white", width: "32px" }} />
                     <p style={{ color: "#bdbdbdff" }}>Задания</p>
                     <p style={{ color: "#bdbdbdff" }}>Рейтинг</p>
                 </div>
                 <div className="header-container__user-info">
                     {user.id !== 0 ?
-                        <Link to={"/signout"}>
-                            <p>{user.username}</p>
-                        </Link>
+                        <>
+                            <Link className="header-container__user-info__item" to={"/profile"}>
+                                <p>{user.username}</p>
+                                <ProfileIcon style={{ fill: "white", stroke: "white", height: "32px" }} />
+                            </Link>
+                        </>
                         :
                         <>
-                            <Link to={"/signin"}>
-                                <p>Войти</p>
-                            </Link>
-                            <Link to={"/signup"}>
-                                <p>Зарегистрироваться</p>
-                            </Link>
+                            {isMobile ?
+                                <Link className="header-container__user-info__item" to={"/signin"}>
+                                    <p>Войти</p>
+                                    <ProfileIcon style={{ fill: "white", stroke: "white", height: "32px" }} />
+                                </Link>
+                                :
+                                <>
+                                    <Link className="header-container__user-info__item" to={"/signin"}>
+                                        <p>Войти</p>
+                                    </Link>
+                                    <p>|</p>
+                                    <Link to={"/signup"}>
+                                        <p>Зарегистрироваться</p>
+                                    </Link>
+                                    <Link className="header-container__user-info__item" to={"/signin"}>
+                                        <ProfileIcon style={{ fill: "white", stroke: "white", height: "32px" }} />
+                                    </Link>
+                                </>
+                            }
                         </>
                     }
                 </div>
